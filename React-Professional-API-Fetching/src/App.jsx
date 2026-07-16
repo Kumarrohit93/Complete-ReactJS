27f973b313324fe9a60219b251f62c0bee4c58e1
Kumarrohit93 <rk4840369@gmail.com>
2026-07-16 15:33:15 +0530

    API fetching Syntax

---
+++ React-Professional-API-Fetching/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AllUsers from "./Components/AllUsers";
+
+const App = () => {
+  return (
+    <div>
+      <AllUsers />
+    </div>
+  );
+};
+
+export default App;
